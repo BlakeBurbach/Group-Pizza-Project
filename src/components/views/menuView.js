821c2f5736efbc70443490da13eda1de548d0aa8
Melody Massard <melody.massard8988@gmail.com>
2018-04-20 10:29:27 -0500

    starting admin view

--- src/components/views/menuView.js
+++ src/components/views/menuView.js
@@ -17,30 +17,34 @@ class Menu extends Component {
         })
     }
 
-    handleAdd = (event) => {
+    handleAddClick = () => {
+      return (event, pizzaId) => {
         console.log('handle add', event.target.value)
         let pizzaToSend = JSON.parse(event.target.value)
         return this.props.dispatch({
           type: 'ADD_PIZZA',
           payload: pizzaToSend
         })
+    }
   }
 
 
-    handleSubtractClick = (event) => {
+    handleSubtractClick = () => {
+       return (event) => {
         console.log('handle subtract', event.target.value)
         let pizzaToSend = JSON.parse(event.target.value)
         return this.props.dispatch({
-          type: 'DELETE_PIZZA',
+          type: 'REMOVE_PIZZA',
           payload: pizzaToSend
         })
+      }
     }
 
     
     render() {
       let pizzaDisplay = this.props.reduxState.pizzaMenu.map((pizza)=> {
 
-      return (<div key = {pizza.id}><p>{pizza.name}</p> <pre>{pizza.description}</pre> <pre>{pizza.cost}</pre><button value={JSON.stringify(pizza)} onClick={this.handleAdd}>+</button>Pizza<button value={JSON.stringify(pizza)} onClick={this.handleSubtract}>-</button></div>)
+      return (<div key = {pizza.id}><p>{pizza.name}</p> <pre>{pizza.description}</pre> <pre>{pizza.cost}</pre><button value={JSON.stringify(pizza)} onClick={this.handleAdd}>+</button>Pizza<button value={pizza.id} onClick={this.handleSubtract}>-</button></div>)
 
 
       })
